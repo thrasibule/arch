b16d7ff153a09974e1074407fa15887e168c3d45
Guillaume Horel <guillaume.horel@gmail.com>
2017-07-19 15:57:14 -0400

    analytic computation of likelihood

--- test.py
+++ test.py
@@ -4,9 +4,14 @@ import numpy as np
 from arch.univariate.volatility import GARCH
 from arch.univariate.mean import ConstantMean
 from arch.univariate.distribution import Normal
-from arch.univariate.recursions import garch_recursion, dgarch_recursion_vp
+from arch.univariate.recursions import garch_recursion
+from arch.univariate.recursions import dgarch_recursion_vp, dgarch_recursion_mp
+
+from arch import arch_model
 
 from statsmodels.tools.numdiff import approx_fprime
+
+from scipy.optimize import minimize
 
 df = feather.read_dataframe("index_returns.fth")
 y = df.ig.dropna()
@@ -24,23 +29,57 @@ backcast = v.backcast(resids)
 var_bounds = v.variance_bounds(resids)
 v.compute_variance(sv_volatility, resids, sigma2, backcast, var_bounds)
 std_resids = resids / np.sqrt(sigma2)
-
 fresids = np.square(resids)
 sresids = np.sign(resids)
+
 params = np.hstack((am.starting_values(),
                     v.starting_values(resids),
-                    d.starting_values(resids)))
+                    d.starting_values(std_resids)))
 
-def f(params, *args):
-    sigma2 = np.empty_like(fresids)
-    x = (params,) + args[:2] + (sigma2,) + args[2:]
-    garch_recursion(*x)
-    return sigma2
+def loglik(params, am):
+    mu = params[0]
+    sv_volatility = params[1:]
+    resids = am.resids(mu)
+    backcast = am.volatility.backcast(resids)
+    sigma2 = np.zeros_like(resids)
+    var_bounds = am.volatility.variance_bounds(resids)
+    am.volatility.compute_variance(sv_volatility, resids, sigma2, backcast, var_bounds)
+    return 0.5 * np.sum(np.log(2 * np.pi * sigma2) + resids ** 2 / sigma2)
 
-J = approx_fprime(sv_volatility,
-                  f,
-                  args = (fresids, sresids, 1, 0, 1, fresids.shape[0], backcast, var_bounds))
+def dloglik(params, am):
+    mu = params[0]
+    sv_volatility = params[1:]
+    resids = am.resids(mu)
+    backcast = am.volatility.backcast(resids)
+    fresids = abs(resids) ** am.volatility.power
+    sresids = np.sign(resids)
+    sigma2 = np.zeros_like(resids)
+    v.compute_variance(sv_volatility, resids, sigma2, backcast, var_bounds)
+    J_analytical = np.zeros((fresids.shape[0], sv_volatility.shape[0]))
+    dgarch_recursion_vp(sv_volatility, fresids, sresids, J_analytical, sigma2,
+                        am.volatility.p, am.volatility.o, am.volatility.q,
+                        fresids.shape[0], backcast)
+    temp = fresids / np.square(sigma2)
+    dll = J_analytical / sigma2[:,None] - J_analytical * temp[:,None]
+    dloglik_vol = 0.5 * dll.sum(axis=0)
+    dresids = am.dresids(mu)
+    if am.volatility.power == 2.:
+        dfresids = 2 * resids * dresids
+    else:
+        dfresids = am.volatility.power * abs(resids) ** (am.volatility.power - 1) * sresids * dresids
+    dbackcast = am.volatility.dbackcast(resids)
+    dsigma2 = np.zeros_like(resids)
+    dgarch_recursion_mp(sv_volatility, dfresids, dsigma2, am.volatility.p, am.volatility.o,
+                        am.volatility.q, fresids.shape[0], dbackcast)
+    dll = dsigma2 / sigma2 + dfresids / sigma2 - temp * dsigma2
+    return np.hstack([0.5 * dll.sum(axis=0), dloglik_vol])
 
-J_analytical = np.zeros((fresids.shape[0], 3))
-dgarch_recursion_vp(sv_volatility, fresids, sresids, J_analytical, sigma2, 1, 0, 1,
-                    fresids.shape[0], backcast)
+J = approx_fprime(params, loglik, args=(am,))
+
+opt = minimize(loglik, params, jac = dloglik, args=(am,))
+am2 = arch_model(10*y)
+am2.fit()
+
+# df = index_returns(index="IG", series=[27, 28], tenor='5yr')
+# df = df.groupby('date').nth(-1)
+# df = df['spread_return'].dropna()
